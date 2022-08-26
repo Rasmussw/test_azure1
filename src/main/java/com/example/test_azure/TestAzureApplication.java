c7439b25dbe44e5147f92011c58a4eabad715885
rasmuswoldike <rswoldike@gmail.com>
2022-08-26 09:04:40 +0200

    Initial commit

---
+++ src/main/java/com/example/test_azure/TestAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAzureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_azure/TestAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
