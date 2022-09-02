7bbcffd0a2045ef33fe94dba6a966e19753841da
rasmuswoldike <rswoldike@gmail.com>
2022-09-02 10:31:15 +0200

    database connected

---
+++ src/main/java/com/example/test_azure/model/User.java
@@ -0,0 +1,37 @@
+package com.example.test_azure.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Column
+    private String name;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
